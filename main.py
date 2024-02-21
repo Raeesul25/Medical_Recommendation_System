866c2b2fd8ade164abe14c9c0b585f6bcbb2662e
Raeesul <it19991986@my.sliit.lk>
2024-02-21 10:29:00 +0530

    add main.py and statics folder

---
+++ main.py
@@ -0,0 +1,115 @@
+from flask import Flask, request, render_template, jsonify  # Import jsonify
+import numpy as np
+import pandas as pd
+import pickle
+
+# flask app
+app = Flask(__name__)
+
+# load databasedataset===================================
+sym_des = pd.read_csv('datasets/symtoms_df.csv')
+precautions = pd.read_csv("datasets/precautions_df.csv")
+workout = pd.read_csv("datasets/workout_df.csv")
+description = pd.read_csv("datasets/description.csv")
+medications = pd.read_csv('datasets/medications.csv')
+diets = pd.read_csv("datasets/diets.csv")
+
+# load model===========================================
+svc = pickle.load(open('svc.pkl','rb'))
+
+# load symptomps dict and disease lisr
+symptoms_dict = pickle.load(open('symptoms_list.pkl', 'rb'))
+diseases_list = pickle.load(open('diseases_list.pkl', 'rb'))
+
+#============================================================
+# custome and helping functions
+#==========================helper funtions================
+def helper(dis):
+    desc = description[description['Disease'] == dis]['Description']
+    desc = " ".join([w for w in desc])
+
+    pre = precautions[precautions['Disease'] == dis][['Precaution_1', 'Precaution_2', 'Precaution_3', 'Precaution_4']]
+    pre = [col for col in pre.values]
+
+    med = medications[medications['Disease'] == dis]['Medication']
+    med = [med for med in med.values]
+
+    die = diets[diets['Disease'] == dis]['Diet']
+    die = [die for die in die.values]
+
+    wrkout = workout[workout['disease'] == dis] ['workout']
+
+    return desc,pre,med,die,wrkout
+
+
+# Model Prediction function
+def get_predicted_value(patient_symptoms):
+    input_vector = np.zeros(len(symptoms_dict))
+    for item in patient_symptoms:
+        input_vector[symptoms_dict[item]] = 1
+    return diseases_list[svc.predict([input_vector])[0]]
+
+
+
+# creating routes========================================
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Define a route for the home page
+@app.route('/predict', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        symptoms = request.form.get('symptoms')
+        # mysysms = request.form.get('mysysms')
+        # print(mysysms)
+        print(symptoms)
+        if symptoms =="Symptoms":
+            message = "Please either write symptoms or you have written misspelled symptoms"
+            return render_template('index.html', message=message)
+        else:
+
+            # Split the user's input into a list of symptoms (assuming they are comma-separated)
+            user_symptoms = [s.strip() for s in symptoms.split(',')]
+            # Remove any extra characters, if any
+            user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
+            # predict the disease for the user symptoms
+            predicted_disease = get_predicted_value(user_symptoms)
+            # get the recommendations for the predicted disease
+            dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
+
+            my_precautions = []
+            for i in precautions[0]:
+                my_precautions.append(i)
+
+            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des,
+                                   my_precautions=my_precautions, medications=medications, my_diet=rec_diet,
+                                   workout=workout)
+
+    return render_template('index.html')
+
+
+
+# about view funtion and path
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# contact view funtion and path
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+# developer view funtion and path
+@app.route('/developer')
+def developer():
+    return render_template("developer.html")
+
+# about view funtion and path
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
